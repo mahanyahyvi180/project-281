47a13a82c29f985a07f9a3742f7eb0a86062d7ee
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-12-23 21:52:12 +0330

    Add files via upload

---
+++ ConsoleApp143/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp143
+{
+  public class NegativeNumberException:Exception
+    {
+        public NegativeNumberException() : base("Illegal operation for a negative number")
+        {
+        }
+        public NegativeNumberException(string messageValue) : base(messageValue)
+        {
+        }
+        public NegativeNumberException(string messageValue, Exception inner) : base(messageValue, inner)
+        {
+        }
+
+
+    }
+}
